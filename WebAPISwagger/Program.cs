913784298316cc7eba6e0de6c4eeda935f66219d
Jimmie Carlsson <jimpa.c66@gmail.com>
2025-09-10 11:34:01 +0200

    Adjust launchSettings and register the app

--- WebAPISwagger/Program.cs
+++ WebAPISwagger/Program.cs
@@ -1,10 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Register services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -12,6 +15,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
